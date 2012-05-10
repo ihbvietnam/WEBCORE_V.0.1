774723267409d9c13575b5ac6c3cb97d18851c7f
unknown <thanhlx@thanhlx-PC.(none)>
2012-05-10 23:13:18 +0700

    Update multi language

--- CODE/protected/components/views/front-end-menu.php
+++ CODE/protected/components/views/front-end-menu.php
@@ -1,5 +1,5 @@
    <ul>
    <?php foreach ($list_menus as $menu):?>
-            <li class="<?php echo $menu['class'];?>"><a href="<?php echo $menu['url'];?>"><?php echo Language::t($menu['name']);?></a></li>
+            <li class="<?php if(isset($menu['class'])) echo $menu['class'];?>"><a href="<?php echo $menu['url'];?>"><?php echo Language::t($menu['name']);?></a></li>
    <?php endforeach;?>
    </ul>
