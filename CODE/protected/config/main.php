63d6f9adc2f9c9e81803aca909049b32cfb41180
Le Thanh <thanh.le@ihbvietnam.com>
2012-05-10 15:15:48 +0700

    Update Guide

--- CODE/protected/config/main.php
+++ CODE/protected/config/main.php
@@ -71,10 +71,10 @@ return array(
 		*/
 		// uncomment the following to use a MySQL database
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=core',
+			'connectionString' => 'mysql:host=localhost;dbname=phoenix',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'thanhdaica',
 			'charset' => 'utf8',
 		),
 		'authManager'=>array(
